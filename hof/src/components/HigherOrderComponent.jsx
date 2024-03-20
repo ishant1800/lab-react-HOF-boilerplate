47bfd48b26dde89ae6a839b418bb10f78510ffc1
ishant1800 <ishanaryan45@gmail.com>
2024-03-20 10:27:55 +0530

    updated

--- hof/src/components/HigherOrderComponent.jsx
+++ hof/src/components/HigherOrderComponent.jsx
@@ -1,6 +1,4 @@
-// src/components/HigherOrderComponent.jsx
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class HigherOrderComponent extends Component {
   constructor() {
@@ -10,105 +8,121 @@ class HigherOrderComponent extends Component {
         { id: '1', name: 'Joe', user_type: 'Developer', age: 31, years: 11 },
         { id: '2', name: 'Hill', user_type: 'Designer', age: 26, years: 4 },
         { id: '3', name: 'John', user_type: 'Teacher', age: 24, years: 2 },
-        { id: '4', name: 'Sam', user_type: 'Entrepreneur', age: 58, years: 25 },
-        { id: '5', name: 'Jack', user_type: 'Designer', age: 43, years: 18 },
-      ],
+        { id: '4', name: 'Sam', user_type: 'Entreprenuer', age: 58, years: 25 },
+        { id: '5', name: 'Jack', user_type: 'Designer', age: 43, years: 18 }
+      ]
     };
   }
 
-  // Progression 2: List all items
-  
-  renderItems = () => {
-    const data = this.state.userData;
-    const mapRows = data.map((item) => (
+  renderItems() {
+    return this.state.userData.map((item) => (
       <React.Fragment key={item.id}>
         <li className="list-elements">
           <span>Id: {item.id}</span>
-          <span>Name: {item.name}</span>
+          <span>Name : {item.name}</span>
           <span>User Type: {item.user_type}</span>
         </li>
       </React.Fragment>
     ));
-    return mapRows;
-  };
+  }
 
-  // Progression 3: List all data based on UserType
-  renderDataByUserType = (userType) => {
-    const filteredData = this.state.userData.filter((item) => item.user_type === userType);
-    const mapRows = filteredData.map((item) => (
-      <React.Fragment key={item.id}>
-        <li className="list-elements">
-          <span>Id: {item.id}</span>
-          <span>Name: {item.name}</span>
-          <span>User Type: {item.user_type}</span>
-        </li>
-      </React.Fragment>
-    ));
-    return mapRows;
-  };
+  renderBasedOnUserType() {
+    return this.state.userData
+      .filter((item) => item.user_type === "Designer")
+      .map((item) => (
+        <React.Fragment key={item.id}>
+          <li className="list-elements">
+            <span>Id: {item.id}</span>
+            <span>Name : {item.name}</span>
+            <span>User Type: {item.user_type}</span>
+          </li>
+        </React.Fragment>
+      ));
+  }
 
-  // Progression 4: Filter all data starting with the letter 'J'
-  renderDataStartingWithJ = () => {
-    const filteredData = this.state.userData.filter((item) => item.name.startsWith('J'));
-    const mapRows = filteredData.map((item) => (
-      <React.Fragment key={item.id}>
-        <li className="list-elements">
-          <span>Id: {item.id}</span>
-          <span>Name: {item.name}</span>
-          <span>User Type: {item.user_type}</span>
-        </li>
-      </React.Fragment>
-    ));
-    return mapRows;
-  };
+  renderBasedOnLetterJ() {
+    return this.state.userData
+      .filter((item) => item.name[0] === "J")
+      .map((item) => (
+        <React.Fragment key={item.id}>
+          <li className="list-elements">
+            <span>Id: {item.id}</span>
+            <span>Name : {item.name}</span>
+            <span>User Type: {item.user_type}</span>
+          </li>
+        </React.Fragment>
+      ));
+  }
 
-  // Progression 5: Filter data based on age
-  renderDataByAge = (minAge, maxAge) => {
-    const filteredData = this.state.userData.filter((item) => item.age > minAge && item.age <= maxAge);
-    const mapRows = filteredData.map((item) => (
-      <React.Fragment key={item.id}>
-        <li className="list-elements">
-          <span>Id: {item.id}</span>
-          <span>Name: {item.name}</span>
-          <span>User Type: {item.user_type}</span>
-        </li>
-      </React.Fragment>
-    ));
-    return mapRows;
-  };
+  renderBasedOnAge() {
+    return this.state.userData
+      .filter((item) => item.age <= 50 && item.age > 28)
+      .map((item) => (
+        <React.Fragment key={item.id}>
+          <li className="list-elements">
+            <span>Id: {item.id}</span>
+            <span>Name : {item.name}</span>
+            <span>User Type: {item.user_type}</span>
+          </li>
+        </React.Fragment>
+      ));
+  }
 
-  // Progression 6: Find the total experience of the designers
-  calculateTotalExperience = (userType) => {
-    const filteredData = this.state.userData.filter((item) => item.user_type === userType);
-    const totalExperience = filteredData.reduce((acc, item) => acc + item.years, 0);
-    return totalExperience;
-  };
+  renderTotalAgeOfDesigners() {
+    const totalYears = this.state.userData.reduce((accumulator, currentValue) => {
+      if (currentValue.user_type === "Designer") {
+        accumulator += currentValue.years;
+      }
+      return accumulator;
+    }, 0);
 
+    return <span>Total years of designers: {totalYears}</span>;
+  }
 
   render() {
-
     return (
-      <div>
-        <h2>PROGRESSION 1 | DEFINE THE STATE</h2>
-        <p>Your task in this iteration is just to create an array of objects inside the state.</p>
+      <>
+        <React.Fragment>
+          <h1>Display all items</h1>
+          <div className="display-box">
+            <ul>{this.renderItems()} </ul>
+          </div>
+        </React.Fragment>
 
-        <h2>PROGRESSION 2 | LIST ALL ITEMS</h2>
-        <ul>{this.renderItems()}</ul>
+        <React.Fragment>
+          <h1>Display based on user type</h1>
+          <div className="display-box">
+            <ul>{this.renderBasedOnUserType()} </ul>
+          </div>
+        </React.Fragment>
 
-        <h2>PROGRESSION 3 | LIST ALL DATA BASED ON USERTYPE</h2>
-        <ul>{this.renderDataByUserType('Designer')}</ul>
+        <React.Fragment>
+          <h1>Filter all data starting with letter J</h1>
+          <div className="display-box">
+            <ul>{this.renderBasedOnLetterJ()} </ul>
+          </div>
+        </React.Fragment>
 
-        <h2>PROGRESSION 4 | FILTER ALL DATA STARTING WITH THE LETTER J</h2>
-        <ul>{this.renderDataStartingWithJ()}</ul>
+        <React.Fragment>
+          <h1>Filter all data based on age greater than 28 and age less than or equal to 50</h1>
+          <div className="display-box">
+            <ul>{this.renderBasedOnAge()} </ul>
+          </div>
+        </React.Fragment>
 
-        <h2>PROGRESSION 5 | FILTER DATA BASED ON AGE</h2>
-        <ul>{this.renderDataByAge(28, 50)}</ul>
-
-        <h2>PROGRESSION 6 | FIND THE TOTAL EXPERIENCE OF THE DESIGNERS</h2>
-        <p>Total experience of designers: {this.calculateTotalExperience('Designer')} years</p>
-      </div>
+        <React.Fragment>
+          <h1>Find the total years of the designers</h1>
+          <div className="display-box">
+            <ul>{this.renderTotalAgeOfDesigners()} </ul>
+          </div>
+        </React.Fragment>
+      </>
     );
   }
+}
+
+function demo() {
+  return "something";
 }
 
 export default HigherOrderComponent;
